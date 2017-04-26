2174efd2a20239d02e96f1561f8fabe394149735
Ellen Duong <ellen@levyvlan502.1418.wlan.wireless-resnet.upenn.edu>
2017-04-25 21:14:31 -0400

    Added Skybox

--- src/main.js
+++ src/main.js
@@ -3,22 +3,20 @@ require('three-lut')
 
 import Framework from './framework'
 
+
+// Reference
+// Skybox: https://threejs.org/examples/?q=light#webgl_lights_hemisphere
+
+// Constants
 const INFINITY = 1.7976931348623157E+10308;
 const PI = 3.14159265358979323;
 
+// Clock
 var startTime = 0.0;
 var prevTime = 0.0;
 var currentTime = 0.0;
-var deltaTime = new THREE.Vector3(0.0, 0.0, 0.0);
 
-// Color Look Up Table
-const colors = 100;
-const mode = 'rainbow';
-const lookupTable = new THREE.Lut(mode, colors);
-
-const WHITE = new THREE.Color("rgb(255, 255, 255)"); 
-const RED = new THREE.Color("rgb(255, 0, 0)");
-
+// GUI
 var options = {
   water: {
     width: 500.0,
@@ -28,7 +26,8 @@ var options = {
   }
 }
 
-var planeMaterial = new THREE.ShaderMaterial({
+// Water Material
+var waterMaterial = new THREE.ShaderMaterial({
   uniforms: {
     u_amount: {
       type: "f",
@@ -68,6 +67,7 @@ var planeMaterial = new THREE.ShaderMaterial({
     },
     direction: {
       type: "v2v",
+      // Random generated directions generated in matlab...
       value: [new THREE.Vector2(0, 1), 
               new THREE.Vector2(1, 0),
               new THREE.Vector2(1, 1),
@@ -92,29 +92,82 @@ function onLoad(framework) {
   var gui = framework.gui;
   var stats = framework.stats;
 
-  // initialize a simple box and material
+  // LIGHTS ------------------- /
   var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
   directionalLight.color.setHSL(0.1, 1, 0.95);
   directionalLight.position.set(1, 3, 2);
   directionalLight.position.multiplyScalar(10);
-  scene.add(directionalLight);
+  // scene.add(directionalLight);
 
-  planeMaterial.uniforms.light.value = directionalLight.position;
+  var dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+  dirLight.color.setHSL( 0.1, 1, 0.95 );
+  dirLight.position.set( -1, 1.75, 1 );
+  dirLight.position.multiplyScalar( 50 );
+  scene.add( dirLight );
+
+
+  // Hemisphere Light
+  var hemiLight = hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+  hemiLight.color.setHSL( 0.6, 1, 0.6 );
+  hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+  hemiLight.position.set( 0, 500, 0 );
+  scene.add( hemiLight );
+
+
+  // END LIGHTS ----------------/
+
+  // waterMaterial.uniforms.light.value = directionalLight.position;
 
   // set camera position
-  camera.position.set(0, 500, 0);
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
+  camera.position.set(0, 0, 250);
+  camera.lookAt(new THREE.Vector3(0,0,0));
 
   // Water
-  var planeGeo = new THREE.PlaneGeometry(options.water.width, 
+  var waterGeo = new THREE.PlaneGeometry(options.water.width, 
                                          options.water.height, 
                                          options.water.widthSegments, 
                                          options.water.heightSegments);
-  //var planeMaterial = new THREE.MeshBasicMaterial( {color: 0x0000ff, side: THREE.DoubleSide, wireframe: true});
-  var planeMesh = new THREE.Mesh(planeGeo, planeMaterial);
-  planeMesh.rotateX(-Math.PI / 2.0);
-  planeMesh.position.set(0, 0, 0);
-  scene.add(planeMesh);
+  //var waterMaterial = new THREE.MeshBasicMaterial( {color: 0x0000ff, side: THREE.DoubleSide, wireframe: true});
+  var waterMesh = new THREE.Mesh(waterGeo, waterMaterial);
+  waterMesh.rotateX(-Math.PI / 2.0);
+  waterMesh.position.set(0, 0, 0);
+  scene.add(waterMesh);
+
+
+  // // TODO: Remove Test Box
+  var boxGeo = new THREE.BoxGeometry(10, 10, 10);
+  var boxMaterial = new THREE.MeshLambertMaterial({color: 0x42f465});
+  var boxMesh = new THREE.Mesh(boxGeo, boxMaterial);
+  scene.add(boxMesh);
+
+
+  // SKY BOX ----------------------/
+  // // GROUND
+  var groundGeo = new THREE.PlaneBufferGeometry( 10000, 10000 );
+  var groundMat = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0x050505 } );
+  groundMat.color.setHSL( 0.095, 1, 0.75 );
+  var ground = new THREE.Mesh( groundGeo, groundMat );
+  ground.rotation.x = -Math.PI/2;
+  ground.position.y = -33;
+  scene.add( ground );
+  ground.receiveShadow = true;
+
+  // SKYDOME
+  var skyMat = new THREE.ShaderMaterial({
+    uniforms: {
+      topColor:    { value: new THREE.Color( 0x0077ff ) },
+      bottomColor: { value: new THREE.Color( 0xffffff ) },
+      offset:      { value: 33 },
+      exponent:    { value: 0.6 }
+    },
+    side: THREE.DoubleSide,
+    vertexShader: require('./shaders/sky-vert.glsl'),
+    fragmentShader: require('./shaders/sky-frag.glsl')
+  });
+  skyMat.uniforms.topColor.value.copy( hemiLight.color );
+  var skyGeo = new THREE.SphereGeometry( 4000, 32, 15 );
+  var sky = new THREE.Mesh( skyGeo, skyMat );
+  scene.add( sky );
 
   // Set up time
   startTime = Date.now();
@@ -127,9 +180,6 @@ function onLoad(framework) {
   fcamera.add(camera, 'fov', 0, 180).onChange(function(newVal) {
     camera.updateProjectionMatrix();
   });
-
-  console.log(camera);
-  console.log("here");
 }
 
 
@@ -139,7 +189,7 @@ function onUpdate(framework) {
   currentTime = Date.now();
 
   // Update time in water shader
-  planeMaterial.uniforms.time.value = (currentTime - startTime) / 1000;
+  waterMaterial.uniforms.time.value = (currentTime - startTime) / 1000;
 
 
 }
