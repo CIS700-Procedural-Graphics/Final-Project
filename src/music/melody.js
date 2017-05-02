42ea9580ab2342e6af4ce0e103b5b0540dfbc3c8
Brian Tong <brktong@gmail.com>
2017-05-01 21:22:02 -0400

    note type enum

--- src/music/melody.js
+++ src/music/melody.js
@@ -6,7 +6,7 @@ import euclid from './../utils/euclid.js'
 
 export default function generateMelody( scaleNote, randomVar ) {
 	var s = tonal.scale.get('major', scaleNote);
-	
+
 
 	// var sO = tonal.note.fromMidi( tonal.note.midi( scaleNote ) - 12 );
 	// s = tonal.scale.get( 'major pentatonic', sO ).concat( s );
@@ -49,6 +49,25 @@ export default function generateMelody( scaleNote, randomVar ) {
 	return finalNotes;
 }
 
+var noteType = {
+	empty: 0,
+	anchor: 1,
+	hook: 2,
+	flair: 3,
+};
+
+function insertHook(melody) {
+
+	for (var i = 0; i < melody.length; i++) {
+		var note = melody[i];
+		if (note.type !== noteType.anchor) {
+			
+		}
+	}
+
+}
+
+
 function createMelodicContour( seed, range ) {
 
 	// Either create a music contour from a smooth noise function
@@ -80,7 +99,7 @@ function createMelodicContour( seed, range ) {
 			}
 		}
 	} else {
-		// In this case, use a random sequence of numbers to determine the slope 
+		// In this case, use a random sequence of numbers to determine the slope
 		// of each segment of 4-8 notes
 		var subsampledContour = [];
 		var duration = ( Math.floor( Math.random() * 2 + 1 ) * 2 + 6 ) * 4;
@@ -103,7 +122,7 @@ function createMelodicContour( seed, range ) {
 			sign = Math.pow(-1, sCounter);
 		}
 	}
-	
+
 
 
 	return subsampledContour;
@@ -180,7 +199,7 @@ function rhythmSum( rhythm ) {
 }
 
 function variateMelody( melody, scale ) {
-	// Let's assume we have a rhythmic melody and assume that the melody 
+	// Let's assume we have a rhythmic melody and assume that the melody
 	// parameter here represents 1 repeat of the theme.
 	// Then, we add some variations to the music.
 
@@ -189,7 +208,7 @@ function variateMelody( melody, scale ) {
 	var start4 = Math.floor( melody.length / 3 );
 	for ( var i = start4; i < melody.length; i++ ) {
 		if ( melody[i].note < 0 ) { continue; }
-		
+
 		var r = Math.random();
 		if ( r > 0.35 - 0.5 * (start4 - i) ) {
 			melody[i].time = 4;
@@ -200,7 +219,7 @@ function variateMelody( melody, scale ) {
 	var start8 = Math.floor( melody.length / 3 * 2 );
 	for ( var i = start8; i < melody.length; i++ ) {
 		if ( melody[i].note < 0 ) { continue; }
-		
+
 		var r = Math.random();
 		if ( r > 0.5 - 0.5 * (start8 - i) ) {
 			melody[i].time = 8;
