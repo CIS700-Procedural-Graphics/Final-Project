c0a69f0810546463a746c5b1f8ee5b7d7475b390
sarahforcier <sforcier@seas.upenn.edu>
2017-07-31 23:58:40 -0700

    reword

--- warning.js
+++ warning.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var os = navigator.platform;
     if (os === 'Linux i686' || os === 'Linux armv7l') {
-        alert("Please note that graphics packages do not properly run on Linux");
+        alert("Please note that this graphics packages may not run properly on Linux");
     }
 });
